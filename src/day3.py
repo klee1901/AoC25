b1daa66b4e612c29b35c4cc40828f41d3f7c6e49
klee1901 <kieranjameslee@hotmail.co.uk>
2025-12-27 17:30:34 +0000

    Day 3: Part 2 solution

--- src/day3.py
+++ src/day3.py
@@ -1,12 +1,26 @@
 import os
 
-def maximiseJoltage(bankOfJoltages):
+def maximiseJoltage(bankOfJoltages, batteriesToSwitchOn):
+    """
+    Input
+    -----
+    bankOfJoltages : list
+        integer values
+    batteriesToSwitchOn : int
+        Number (<= length bankOfJoltages) of joltages that can be selected
+    Output
+    ------
+    int
+        Maximum output joltage than can be acheived by selecting batteriesToSwitchOn joltages
+    """
+    if batteriesToSwitchOn == 1:
+        return max(bankOfJoltages)
+    # Maximum joltage that can be added by turning on a single battery
+    maxJoltage = max(bankOfJoltages[:1-batteriesToSwitchOn])
+    startOfRemaining = bankOfJoltages.index(maxJoltage) + 1
+    maxAdditionalJoltage = maximiseJoltage(bankOfJoltages[startOfRemaining:], batteriesToSwitchOn-1)
 
-    maxTens = max(bankOfJoltages[:-1])
-    startOfPotentialUnits = bankOfJoltages.index(maxTens) + 1
-    maxUnit = max(bankOfJoltages[startOfPotentialUnits:])
-
-    return 10*maxTens + maxUnit
+    return maxJoltage* 10**(batteriesToSwitchOn-1) + maxAdditionalJoltage
 
 script_path = os.path.dirname(__file__)
 rel_filepath = "../Input/day3.txt"
@@ -15,7 +29,8 @@ input_filepath = os.path.join(script_path, rel_filepath)
 with open(input_filepath) as file:
     banks = file.readlines()
 joltages = [[int(joltage) for joltage in line.strip()] for line in banks]
-maxJoltages = [maximiseJoltage(bank) for bank in joltages]
+maxJoltages2Batteries = [maximiseJoltage(bank,2) for bank in joltages]
+maxJoltages12Batteries = [maximiseJoltage(bank,12) for bank in joltages]
 
-print("Part 1: ", sum(maxJoltages))
-print("Part 2: ", "TBD")
+print("Part 1: ", sum(maxJoltages2Batteries))
+print("Part 2: ", sum(maxJoltages12Batteries))
