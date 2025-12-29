aab6381ffe383474c382d9cf9c69f62eeaf790a6
klee1901 <kieranjameslee@hotmail.co.uk>
2025-12-29 10:23:00 +0000

    Day 7: Part 1 solution

---
+++ src/day7.py
@@ -0,0 +1,27 @@
+import os
+
+script_path = os.path.dirname(__file__)
+rel_filepath = "../Input/day7.txt"
+input_filepath = os.path.join(script_path, rel_filepath)
+
+with open(input_filepath) as file:
+    lines = file.readlines()
+beams = [lines[0].index('S')]
+rowNum = 0
+splits = 0
+while rowNum < len(lines):
+    nxtBeams = []
+    for beam in beams:
+        if lines[rowNum][beam] == '^':
+            splits += 1
+            if beam - 1 not in nxtBeams:
+                nxtBeams.append(beam - 1)
+            nxtBeams.append(beam + 1)
+        else:
+            if beam not in nxtBeams:
+                nxtBeams.append(beam)
+    rowNum += 1
+    beams = nxtBeams
+
+print("Part 1: ", splits)
+print("Part 2: ", "TBD")
